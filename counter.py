2f1208b9aec5fea64de51fbf9f113e9b6cb5c709
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2023-03-06 10:02:08 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -2,3 +2,30 @@ import time
 import streamlit as st
 
 st.set_page_config(page_title="Dofus incrément", layout="wide",page_icon = 'ico.png')
+
+st.markdown(
+    """<style>
+    .css-15zrgzn {display: none}
+    .css-eczf16 {display: none}
+    .css-jn99sy {display: none}
+    .css-10trblm {color : #83C9FF;}
+    .css-1yk9tp8 {display: none}
+    button[title="View fullscreen"]{visibility: hidden;}
+    table
+    {
+        width: 100%;
+        color: #68BEBA;
+        text-align:right;
+        background-color: #011b32;
+    }
+    footer {visibility: hidden;}
+    </style>""", unsafe_allow_html=True)
+col1,col2=st.columns([0.08,2])
+with col1:
+    st.markdown('')
+    st.markdown('')
+    st.image('ico1.png', use_column_width='auto')
+    
+with col2:
+  st.markdown(f'<p style="color:#83C9FF;font-size:75px;">{"Dofus incrément"}</p>', unsafe_allow_html=True)
+
