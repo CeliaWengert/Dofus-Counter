192c2fb25733c1f7277c561e1d2cd67292590858
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2023-03-06 13:52:13 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -75,7 +75,7 @@ select2=st.selectbox('Selection du challenge',df_chall)
 
 #build_sidebar(conn)
 
-if st.button('Incrément'):
+if st.button('Incrément !'):
     request = 'INSERT INTO inc(Name,Date,Challenge_Name,Challenge_count) VALUES({select1},dt.now(pytz.timezone(\'Europe/Paris\')),{select2},1);'
     conn.execute(request)
     conn.commit()
