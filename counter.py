2b4144c79c27699d34537bb2b72a17b78c923938
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2023-03-06 15:44:33 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -78,7 +78,7 @@ if st.button('Incrément !'):
 chart_data = pd.read_sql_query("SELECT Count(*) as Noob_Counter, Name from inc group by Name ", conn)
 st.bar_chart(chart_data,x="Name",y="Noob_Counter")
 
-pie_data=pd.eread_sql_query("SELECT Count(*) as Chall_Counter, Name from inc group by Challenge_name ", conn)
+pie_data=pd.read_sql_query("SELECT Count(*) as Chall_Counter, Name from inc group by Challenge_name ", conn)
 fig = px.pie(pie_data, values='Chall_Counter', names='Challenge_name')
 st.plotly_chart(fig,use_container_width=True)
 
