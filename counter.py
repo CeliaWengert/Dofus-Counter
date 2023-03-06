7f51a7b02f8e7803d1d4bb7059d1a75ae07145b2
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2023-03-06 12:06:13 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -5,12 +5,13 @@ import pytz
 
 import sqlite3
 conn = sqlite3.connect('counter.db')
-c = conn.cursor()
+
 
     
 request='CREATE TABLE IF NOT EXISTS inc(Name TEXT NOT NULL, Date TEXT NOT NULL, Challenge_name TEXT, Challenge_count INTEGER, table_constraints) '
 
-c.execute(request)
+conn.execute(request)
+conn.commit()
 
 st.set_page_config(page_title="Dofus incrément", layout="wide",page_icon = 'ico.png')
 
@@ -40,10 +41,10 @@ with col1:
 with col2:
   st.markdown(f'<p style="color:#83C9FF;font-size:75px;">{"Dofus incrément"}</p>', unsafe_allow_html=True)
 
-
 if st.button('Incrément'):
-    request = 'INSERT into inc(Name,Date,Challenge_Name,Challenge_count) VALUES("Hugo",dt.now(pytz.timezone(\'Europe/Paris\')),"Nomade",1);'
-    c.execute(request)
+    request = 'INSERT INTO inc(Name,Date,Challenge_Name,Challenge_count) VALUES("Hugo",dt.now(pytz.timezone(\'Europe/Paris\')),"Nomade",1);'
+    conn.execute(request)
+    conn.commit()
 else:
     st.write('coinc')
 
