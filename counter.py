02fab91214da69156a208480b169836d72b51412
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2023-03-07 12:31:52 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -101,7 +101,7 @@ hide_table_row_index = """
 st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
 df=pd.read_sql_query("SELECT * from inc order by ROWID DESC", conn)
-st.dataframe(df,use_container_width=true)
+st.dataframe(df,use_container_width='true')
 
     
 
